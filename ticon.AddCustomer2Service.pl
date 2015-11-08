a31635fe52f1bf20761e60f973630c510a5e63c7
root <joserribeiro26@gmail.com>
2015-11-08 02:12:44 +0000

    Adicionado corrigido

--- ticon.AddCustomer2Service.pl
+++ ticon.AddCustomer2Service.pl
@@ -1,11 +1,13 @@
+#!/usr/bin/perl
+#
 use strict;
 use warnings;
 
 use File::Basename;
 use FindBin qw($RealBin);
 use lib dirname($RealBin);
-use lib dirname($RealBin) . ‘/Kernel/cpan-lib’;
-use lib dirname($RealBin) . ‘/Custom’;
+use lib dirname($RealBin) . '/Kernel/cpan-lib';
+use lib dirname($RealBin) . '/Custom';
 
 use Getopt::Std;
 
@@ -13,28 +15,28 @@ use Kernel::System::ObjectManager;
 
 # create object manager
 local $Kernel::OM = Kernel::System::ObjectManager->new(
-‘Kernel::System::Log’ => {
-LogPrefix => ‘OTRS-otrs.AddCustomer2Service’,
+'Kernel::System::Log' => {
+LogPrefix => 'OTRS-otrs.AddCustomer2Service',
 },
 );
 
-my $ServiceObject = $Kernel::OM->Get(‘Kernel::System::Service’);
+my $ServiceObject = $Kernel::OM->Get('Kernel::System::Service');
 
 # get options
 my %Opts;
-getopt( ‘su’, \%Opts );
+getopt( 'su', \%Opts );
 
 if ( $Opts{h} || !$Opts{u} || !$Opts{s} ) {
-print “otrs.AddCustomer2Service.pl – add customer to a service\n”;
-print “Copyright (C) 2001-2015 OTRS AG, http://otrs.com/\n”;
-print “usage: otrs.AddCustomer2Service.pl -u customerlogin -s servicename \n”;
+print "otrs.AddCustomer2Service.pl – add customer to a service\n";
+print "Copyright (C) 2001-2015 OTRS AG, http://otrs.com/\n";
+print "usage: otrs.AddCustomer2Service.pl -u customerlogin -s servicename \n";
 exit 1;
 }
 
 my %Param = (
-UserID => ‘1’,
-Active => ‘1’,
-ServiceID => ”,
+UserID => '1',
+Active => '1',
+ServiceID => '',
 CustomerUserLogin => $Opts{u},
 Service => $Opts{s},
 );
@@ -45,8 +47,8 @@ Name => $Param{Service},
 
 $ServiceObject->CustomerUserServiceMemberAdd(%Param);
 
-print “ID Service: $Param{ServiceID}/ Service: $Param{Service} \n”;
-print “Customer: $Param{CustomerUserLogin}\n”;
+print "ID Service: $Param{ServiceID}/ Service: $Param{Service} \n";
+print "Customer: $Param{CustomerUserLogin}\n";
 
 exit 0;
 
